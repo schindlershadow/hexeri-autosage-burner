7e2dffe88b05073090f20dbd7fd9a1638df41d42
Schindler <schindlershadow@gmail.com>
2025-07-23 16:12:12 -0400

    forgot to define frontItem

--- startup.lua
+++ startup.lua
@@ -1,19 +1,21 @@
 auto = peripheral.wrap("left")
-while true do 
+while true do
     turtle.select(1)
     if turtle.getItemDetail() == nil or turtle.getItemDetail().name ~= "minecraft:flint_and_steel" then
         print("FLINT AND STEEL NOT FOUND")
         print("Please place a flint and steel into the first slot")
     else
-    if frontItem ~= nil then
-        item = frontItem.getItemDetail(1)
-        if item ~= nil then
-            print("Using flint and steel")
-            if item.damage == 0 then
-                auto.useOnBlock()
+        frontItem = peripheral.wrap("front")
+        if frontItem ~= nil then
+            item = frontItem.getItemDetail(1)
+            --print(tostring(item))
+            if item ~= nil then
+                if item.damage == 0 then
+                    print("Using flint and steel")
+                    auto.useOnBlock()
+                end
             end
         end
-    end
     end
     sleep(1)
 end
