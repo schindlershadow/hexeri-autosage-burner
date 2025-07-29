431ecbce1c946ebe350b7dcb9873e54290e52683
Schindler <schindlershadow@gmail.com>
2025-07-29 19:38:32 -0400

    update to autofuel

--- startup.lua
+++ startup.lua
@@ -1,21 +1,63 @@
-auto = peripheral.wrap("left")
-while true do
-    turtle.select(1)
-    if turtle.getItemDetail() == nil or turtle.getItemDetail().name ~= "minecraft:flint_and_steel" then
-        print("FLINT AND STEEL NOT FOUND")
-        print("Please place a flint and steel into the first slot")
-    else
-        frontItem = peripheral.wrap("front")
-        if frontItem ~= nil then
-            item = frontItem.getItemDetail(1)
-            --print(tostring(item))
-            if item ~= nil then
-                if item.damage == 0 then
-                    print("Using flint and steel")
-                    auto.useOnBlock()
-                end
+local auto = peripheral.wrap("left")
+local foundFuelSlot = nil
+local flintAndSteelSlot = nil
+
+local function checkInventory()
+    foundFuelSlot = nil
+    flintAndSteelSlot = nil
+    for slot = 1, 16 do
+        turtle.select(slot)
+        local item = turtle.getItemDetail()
+        if item ~= nil then
+            if item.name == "minecraft:coal" or item.name == "minecraft:lignite" or item.name == "minecraft:charcoal" or item.name == "minecraft:wood" or item.name == "modern_industrialization:lignite_coal" then
+                foundFuelSlot = slot
+                break -- breaks the for loop, so we don't check other slots once found
+            elseif item.name == "minecraft:flint_and_steel" and item.damage ~= 0 then
+                flintAndSteelSlot = slot
+                print("Flint and steel found in slot " .. slot)
             end
         end
     end
-    sleep(1)
+    if foundFuelSlot ~= nil then
+        turtle.select(foundFuelSlot)
+        turtle.refuel()
+        print("Refueled with fuel from slot " .. foundFuelSlot)
+    end
+end
+
+checkInventory()
+
+while true do
+    -- Check all slots for burnable fuel (coal or wood)
+
+    if turtle.getFuelLevel() < 1 then
+        checkInventory()
+    end
+
+    if turtle.getFuelLevel() < 1 then
+        print("NO FUEL FOUND")
+        sleep(5)
+    else
+        local item = turtle.getItemDetail(flintAndSteelSlot)
+        if item ~= nil then
+            if item.name == "minecraft:flint_and_steel" and item.damage ~= 0 then
+                local frontItem = peripheral.wrap("front")
+                if frontItem ~= nil then
+                    local sage = frontItem.getItemDetail(1)
+                    if item ~= nil then
+                        if sage.damage == 0 then
+                            turtle.select(flintAndSteelSlot)
+                            print("Using flint and steel")
+                            auto.useOnBlock()
+                        end
+                    end
+                end
+            end
+        else
+            -- No flint_and_steel found, so we just continue to the next iteration of the loop.
+            print("No fuel or flint_and_steel found")
+        end
+
+        sleep(1)
+    end
 end
